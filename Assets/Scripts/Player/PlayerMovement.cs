ec31edc5694dec9abab5e8eb8d5e7704c87348ac
Zac <zachary.slater@student.uts.edu.au>
2025-03-10 17:40:11 +1100

    As a player, I want to be able to jump to avoid obstacles and enemies

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,20 +4,29 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     [SerializeField]
-    float moveSpeed = 300f;
+    float moveSpeed;
+    [SerializeField]
+    float jumpSpeed;
 
     void Start()
     {
+        Application.targetFrameRate = 60; // JUST FOR TESTING THIS CAN BE REMOVED LATER
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         float xInput = Input.GetAxisRaw("Horizontal");
         if (xInput != 0)
         {
-            Debug.Log(xInput);
-            rb.linearVelocityX = xInput * moveSpeed * Time.deltaTime;
+            rb.linearVelocityX = xInput * moveSpeed;
+        }
+
+        float characterHeightFromCenterToGround = 0.6f; // will need to be updated when changed from default sprite
+        RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position,Vector2.down,characterHeightFromCenterToGround);
+        if (Input.GetKeyDown(KeyCode.Space) && raycastHit2D)
+        {
+            rb.linearVelocityY = jumpSpeed;
         }
     }
 }
