a699f9d1166f173f2eef0addd12c82401d4ff909
JamesK-XIV <jameskidman14@gmail.com>
2025-03-12 12:07:21 +1100

    As a player, I want to be able to see how long I have spent on the game

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     public TMP_Text scoreTxt;
+    public TMP_Text timerTxt;
     private int score;
+    private float timeTracker;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,6 +18,7 @@ public class UIManager : MonoBehaviour
     {
         //test += 1;
         //UpdateScore(test);
+        UpdateTimer();
     }
 
     //Pass in the points earned by the player to add to score and display in UI
@@ -23,5 +26,13 @@ public class UIManager : MonoBehaviour
     {
         score += points;
         scoreTxt.text = ("Score: " + string.Format("{0:0000000}", score));
+    }
+
+    private void UpdateTimer()
+    {
+        timeTracker += Time.deltaTime;
+        int seconds = Mathf.FloorToInt(timeTracker % 60);
+        int minutes = Mathf.FloorToInt(timeTracker / 60);
+        timerTxt.text = ("Time: " + string.Format("{0:00}:{1:00}", minutes, seconds));
     }
 }
